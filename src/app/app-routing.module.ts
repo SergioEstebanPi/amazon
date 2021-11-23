2151b6ef9f0fe7b36a855b2b4cb0816f5e85b364
Sergio Pina <sergio_pina@epam.com>
2021-11-22 21:01:22 -0500

    Configuro rutas generales y del modulo de seguridad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './assets/error/error.component';
+import { IndexComponent } from './assets/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'index',
+    component: IndexComponent,
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/index'
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/error'
+  },
+  {
+    path: 'seguridad',
+    loadChildren: () => import('./modulos/seguridad/seguridad.module').then(m => m.SeguridadModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./modulos/admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'estaciones',
+    loadChildren: () => import('./modulos/estaciones/estaciones.module').then(m => m.EstacionesModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
